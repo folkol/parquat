27100ed9de26d6994ca76df1321d3ce39b9684b2
Matte <mattias4@kth.se>
2024-04-28 14:43:59 +0200

    Added query arg

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,43 @@
 use std::error::Error;
 use std::path::PathBuf;
 
-use clap::{Args, Parser};
+use clap::Parser;
 use polars::prelude::*;
+use polars::sql::SQLContext;
 
-#[derive(Debug, Parser)] // requires `derive` feature
-#[command(name = "git")]
-#[command(about = "A fictional versioning CLI", long_about = None)]
+#[derive(Debug, Parser)]
+#[command(name = "pcat")]
+#[command(about = "cat for .parquet", long_about = None)]
 struct Pcat {
-    /// Stuff to add
     #[arg(required = true)]
     files: Vec<PathBuf>,
+    #[arg(short, long, help = "SELECT `foo.bar`, `foo.baz` FROM t WHERE `foo.qux` <> 1337")]
+    query: Option<String>,
 }
 
-fn main() -> Result<(), Box<dyn Error>> {
+type MainResult = Result<(), Box<dyn Error>>;
+type LazyFrames = Result<Vec<LazyFrame>, PolarsError>;
+
+fn main() -> MainResult {
     let args = Pcat::parse();
-    println!("{:?}", args.files);
-    let mut lfs = vec![];
-    for file in args.files {
-        let result = LazyFrame::scan_parquet(file, ScanArgsParquet::default())?;
-        lfs.push(result);
-    }
-    let df_vertical_concat = concat_lf_diagonal(
-        lfs,
-        UnionArgs::default(),
-    )?.collect()?;
-    let result = df_vertical_concat;
+    let lfs = args.files.iter().map(get_parquet).collect::<LazyFrames>()?;
+    let lfs = concat_lf_diagonal(lfs, UnionArgs::default())?;
+    let lf = match args.query {
+        Some(query) => {
+            let mut context = SQLContext::new();
+            context.register("t", lfs);
+            context.execute(&query)?
+        }
+        _ => lfs,
+    };
+    let result = lf.collect()?;
     println!("{result:?}");
     Ok(())
+}
+
+fn get_parquet(path: &PathBuf) -> PolarsResult<LazyFrame> {
+    let result = LazyFrame::scan_parquet(path, ScanArgsParquet::default());
+    result.map_err(|e: PolarsError| {
+        e.wrap_msg(&|msg| format!("Couldn't read {path:?} ({msg})"))
+    })
 }
