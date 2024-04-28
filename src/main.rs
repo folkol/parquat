14554e377b0425af1e1c204b547f546f917786c8
Matte <mattias4@kth.se>
2024-04-28 15:53:17 +0200

    Added '--full' option and using it by default if stdout isn't tty

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+#![feature(unix_sigpipe)]
 use std::error::Error;
+use std::io::IsTerminal;
 use std::path::PathBuf;
 
 use clap::Parser;
@@ -11,13 +13,20 @@ use polars::sql::SQLContext;
 struct Pcat {
     #[arg(required = true)]
     files: Vec<PathBuf>,
-    #[arg(short, long, help = "SELECT `foo.bar`, `foo.baz` FROM t WHERE `foo.qux` <> 1337")]
+    #[arg(
+        short,
+        long,
+        help = "SELECT `foo.bar`, `foo.baz` FROM t WHERE `foo.qux` <> 1337"
+    )]
     query: Option<String>,
+    #[arg(short, long, help = "Show full output (do not condense)")]
+    full: bool,
 }
 
 type MainResult = Result<(), Box<dyn Error>>;
 type LazyFrames = Result<Vec<LazyFrame>, PolarsError>;
 
+#[unix_sigpipe = "sig_dfl"]
 fn main() -> MainResult {
     let args = Pcat::parse();
     let lfs = args.files.iter().map(get_parquet).collect::<LazyFrames>()?;
@@ -31,13 +40,20 @@ fn main() -> MainResult {
         _ => lfs,
     };
     let result = lf.collect()?;
-    println!("{result:?}");
+
+    if args.full || !std::io::stdout().is_terminal() {
+        std::env::set_var("POLARS_FMT_TABLE_FORMATTING", "NOTHING");
+        std::env::set_var("POLARS_FMT_MAX_ROWS", "-1");
+        std::env::set_var("POLARS_FMT_MAX_COLS", "-1");
+        println!("{result:?}");
+    } else {
+        println!("{result:?}");
+    }
+
     Ok(())
 }
 
 fn get_parquet(path: &PathBuf) -> PolarsResult<LazyFrame> {
     let result = LazyFrame::scan_parquet(path, ScanArgsParquet::default());
-    result.map_err(|e: PolarsError| {
-        e.wrap_msg(&|msg| format!("Couldn't read {path:?} ({msg})"))
-    })
+    result.map_err(|e: PolarsError| e.wrap_msg(&|msg| format!("Couldn't read {path:?} ({msg})")))
 }
